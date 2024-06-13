25c2598d5ac4482364a0e518ac297e0ab0c082f0
Akansha Salwan <akankshasalwan@gmail.com>
2024-06-14 01:30:32 +0530

    updates all

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./components/hero";
+import Navigation from "./components/navigation";
+
+const App = () => {
+
+  return <div>
+    <Navigation></Navigation>
+    <HeroSection></HeroSection>
+  </div>
+
+};
+export default App;
